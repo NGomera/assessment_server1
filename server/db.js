7d3b3418c89416f1425c1c494ba4ffa3d5806c99
Nicko Paul Gomera.github.io <58455064+NGomera@users.noreply.github.com>
2022-01-21 15:45:56 +0800

    Update db.js

--- server/db.js
+++ server/db.js
@@ -1,14 +1,14 @@
 const Pool = require("pg").Pool;
 
 const pool = new Pool({
-  user: "postgres",
-  password: "postgres",
-  host: "localhost",
+  user: "gvmtaizqrtpxds",
+  password: "750c35638fe0ea0dacd267ec1914de01a798e5642617b3ba64cbc1c650b212b9",
+  host: "ec2-44-194-112-166.compute-1.amazonaws.com",
   port: 5432,
-  database: "foods",
-  //   ssl: {
-  //     rejectUnauthorized: false,
-  //   },
+  database: "dbct8fgkrhmsbe",
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 module.exports = pool;
